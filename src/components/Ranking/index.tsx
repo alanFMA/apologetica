b886c19ca8c20aa8e01aeef73777283ae30e8153
AlanFMA <alanmonteiro105@gmail.com>
2024-07-13 11:18:13 -0300

    adicionando coroa ranking

--- src/components/Ranking/index.tsx
+++ src/components/Ranking/index.tsx
@@ -1,26 +1,6 @@
 import React from 'react';
 import { useMediaQuery } from '@mui/material';
 import { Aluno, Description, Ranking, Titulo } from './styles';
-import Pedro from '../../assets/001-pedro-torres.jpg';
-import Lizandra from '../../assets/006-lizandra.png';
-import Eliabe from '../../assets/003-eliabe.png';
-import Lucila from '../../assets/002-lucila.png';
-import Jonas from '../../assets/004-jonas-william.png';
-import Rafael from '../../assets/005-rafael.png';
-import Lucas from '../../assets/007-lucas.png';
-import Cintia from '../../assets/008-cintia.jpeg';
-import Dominick from '../../assets/009-dominick.jpeg';
-import Annita from '../../assets/010-annita.png';
-import Gabriel from '../../assets/011-gabriel.png';
-import Jezreel from '../../assets/012-jezreel.png';
-import Jaysa from '../../assets/013-jaysa.png';
-import Laura from '../../assets/014-laura.png';
-import Queila from '../../assets/015-queila.png';
-import Kesia from '../../assets/016-kesia.jpeg';
-import MariaKlara from '../../assets/017-maria-klara.png';
-import Victor from '../../assets/018-victor.png';
-import Giglyene from '../../assets/019-giglyene.png';
-import Generic from '../../assets/generic-avatar.jpg';
 import alunosOrdenados from '../../data';
 
 export default function RankingList() {
--- src/components/Ranking/styles.ts
+++ src/components/Ranking/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Crown from '../../assets/crown.png';
 
 export const Titulo = styled.h2`
   text-align: center;
@@ -12,10 +13,12 @@ export const Ranking = styled.div`
 `;
 
 export const Aluno = styled.div`
+  position: relative; /* Adicionado para posicionamento relativo do ::before */
   display: flex;
   align-items: center;
   margin: 15px 0;
   width: 70%;
+
   img {
     width: 100px;
     height: 100px;
@@ -27,6 +30,20 @@ export const Aluno = styled.div`
     img {
       border: 5px solid #ffd700;
     }
+
+    &::before {
+      content: '';
+      display: block;
+      width: 55px;
+      height: 40px;
+      background-image: url(${Crown});
+      background-size: contain;
+      background-repeat: no-repeat;
+      position: absolute;
+      top: -25px; /* ajuste conforme necessário */
+      left: calc(50% - 76px); /* ajuste conforme necessário */
+      transform: translateX(-50%);
+    }
   }
 
   &:nth-child(2) {
@@ -34,6 +51,7 @@ export const Aluno = styled.div`
       border: 5px solid #c0c0c0;
     }
   }
+
   &:nth-child(3) {
     img {
       border: 5px solid #cd7f32;
